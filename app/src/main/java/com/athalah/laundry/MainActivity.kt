74a1468deaf89fcebb57482b739d2b5d30dd731e
Athalah-force <exophvirexophivr@gmail.com>
2025-02-04 07:39:51 +0700

    perubahan background pada dashboard utama,penambahan layout baru yaitu data pelanggan dan layanan pelanggan, serta penambahan kotlin ke layanan dan tambahan

--- app/src/main/java/com/athalah/laundry/MainActivity.kt
+++ app/src/main/java/com/athalah/laundry/MainActivity.kt
@@ -1,20 +1,43 @@
 package com.athalah.laundry
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var cv_layanan: CardView
+    lateinit var cv_tambahan: CardView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        cv_layanan = findViewById(R.id.card3)
+        cv_tambahan = findViewById(R.id.card4)
+
+        cv_layanan.setOnClickListener {
+            val intent = Intent(this, Activity_layanan_pelanggan::class.java)
+            startActivity(intent)
+        }
+
+        cv_tambahan.setOnClickListener {
+            val intent = Intent(this, tambah_pelanggan_Activity::class.java)
+            startActivity(intent)
         }
     }
 }
